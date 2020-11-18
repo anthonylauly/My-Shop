0c0bdc804f16cd4579c4feb7da73c91f1f7b4477
anthony lauly <anthony.lauly1833@gmail.com>
2020-11-18 20:05:44 +0700

    final commit

---
+++ payment/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+import braintree
+import weasyprint
+from io import BytesIO
+
+from orders.models import Order
+
+# Create your views here.
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == 'POST':
+        nonce = request.POST.get('payment_method_nonce', None)  #retrieve nonce
+        result = braintree.Transaction.sale({
+            'amount': '{:.2f}'.format(order.get_total_cost()),
+            'payment_method_nonce': nonce,
+            'options': {
+                'submit_for_settlement': True
+            }
+        })
+        if result.is_success:
+            order.paid = True
+            order.braintree_id = result.transaction.id
+            order.save()
+
+            #Create invoice e-mail
+            subject = 'My Shop - Invoice. {}'.format(order.id)
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+
+            #generate pdf
+            html = render_to_string('orders/order/pdf.html', {'order':order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT+'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out,
+                stylesheets=stylesheets)
+            
+            #attach PDF file
+            email.attach('order_{}.pdf'.format(order.id),
+                out.getvalue(),
+                'application/pdf'
+            )
+
+            #send e-mail
+            email.send()
+            
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+    else:
+        client_token = braintree.ClientToken.generate()
+        return render(request, 'payment/process.html', 
+                        {
+                            'order':order,
+                            'client_token':client_token
+                        })
+
+def payment_done(request):
+    return render(request, 'payment/done.html')
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
+
